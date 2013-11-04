06155f25bcbea863c6040c619af691c0de1e3b71
Roman Morozov <romo@ciklum.com>
2013-11-04 23:16:51 +0200

    updating build

---
+++ build_config/gruntfile.js
@@ -0,0 +1,75 @@
+module.exports = function (grunt) {
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		sources: ['src/*.*'],
+		concat: {
+			options: {
+				separator: ';'
+			},
+			dist: {
+				src: ['src/js/**/*.js'],
+				dest: 'build/<%= pkg.name %>.js'
+			}
+		},
+		uglify: {
+			options: {
+				banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+			},
+			dist: {
+				files: {
+					'build/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+				}
+			}
+		},
+		qunit: {
+			files: ['test/**/*.html']
+		},
+		jshint: {
+			files: ['Gruntfile.js', 'src/js/**/*.js'],
+			jshintrc: '.jshintrc'
+		},
+		watch: {
+			files: ['<%= sources %>'],
+			tasks: ['compass']
+		},
+		compass: {
+			dist: {
+				options: {
+					sassDir: 'src/scss',
+					cssDir: 'build',
+					environment: 'production'
+				}
+			},
+			dev: {}
+		},
+		requirejs: {
+			compile: {
+				options: {
+					name: 'index',
+					baseUrl: "src/js",
+					mainConfigFile: "config.js",
+					out: "build/index.build.js",
+					optimize: 'uglify2',
+					keepBuildDir: true,
+					generateSourceMaps: true,
+					preserveLicenseComments: false
+				}
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-qunit');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-compass');
+	grunt.loadNpmTasks('grunt-contrib-requirejs');
+	grunt.loadNpmTasks('grunt-concurrent');
+
+	grunt.registerTask('test', ['jshint']);
+//  grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify']);
+	grunt.registerTask('default', ['compass', 'concat', 'uglify']);
+
+};

9e5185a47932950ba9c462820bfcf77325bc6d1e
Roman Morozov <romo@ciklum.com>
2013-12-03 11:12:53 +0200

    modueles bash shell script

---
+++ modules_install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+npm install -g grunt
+npm install -g grunt-cli
+
+npm install -g grunt-contrib-jshint
+npm install -g grunt-contrib-csslint
+npm install -g grunt-plato
+npm install -g grunt-contrib-watch
+npm install -g grunt-contrib-compass
+npm install -g grunt-contrib-requirejs
+npm install -g grunt-karma
+npm install -g grunt-jsvalidate
+npm install -g grunt-complexity
+
+npm install -g mocha
+npm install -g karma-mocha
+npm install -g karma-chai
+npm install -g karma-coverage
+npm install -g karma-junit-reporter
+
+npm install -g load-grunt-tasks
+npm install -g jshint-jenkins-checkstyle-reporter
+npm install -g time-grunt
